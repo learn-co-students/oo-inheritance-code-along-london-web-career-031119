6f2baf47fe3e10f1abfaf217bea36afed16c8f45
Fernando <fernando_lopezc@live.com>
2019-03-17 09:13:52 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,15 @@
 require_relative "./vehicle.rb"
+require 'pry'
+# our Car class will need access to the Vehicle class 
+
+#defining the subclass 
+class Car < Vehicle #>we use the arrow to inherit from Vehicle class
+
+def go
+    'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+end
+
+binding.pry
+
+end
+
