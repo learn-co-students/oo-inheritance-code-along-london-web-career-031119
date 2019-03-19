f01c28e62f10516d233569d8f53354f3e75f07c9
github username <github email address>
2019-03-19 23:07:17 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,25 @@
 require_relative "./vehicle.rb"
+
+class Car
+
+    attr_accessor :wheel_number, :wheel_size
+
+    def initialize(wheel_size, wheel_number)
+    end
+
+    def go
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+    def fill_up_tank
+        return "filling up!"
+    end
+    # def wheel_size
+    #     @wheel_size
+    # end
+
+    # def wheel_number
+    #     @wheel_number
+    # end
+
+end
